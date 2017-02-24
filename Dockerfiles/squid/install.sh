aafaf94de430846bfdd743e8928cfe8ad4466c1b
usuario de gerencia <nariga@lenovo.home.jab>
2017-02-24 14:44:36 -0300

    squid gerenciando usando volumes e sarg em /squid

--- Dockerfiles/squid/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp docker-squid.service /etc/systemd/system/docker-squid.service
-systemctl daemon-reload
-systemctl enable docker-squid.service
--- Dockerfiles/squid/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "iniciando squid"
-/usr/sbin/squid3
-sleep 5
-tail -f /var/log/squid3/access.log
